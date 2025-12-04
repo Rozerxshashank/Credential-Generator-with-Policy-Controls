a9f5117bc6cc7f0c9b330a3474131363171c7918
Shashank <yshashank959@gmail.com>
2025-12-05 01:33:21 +0530

    added cors middleware

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,    # <--- No more "*"
+    allow_origins=origins,   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
